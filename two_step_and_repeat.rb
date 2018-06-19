dcf741889b464012017b3ff031942786b3ad3351
anuosu <anuosunnuyi14@gmail.com>
2018-06-19 17:52:23 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -1,5 +1,4 @@
 def first_steps
-  loop do
     puts "Right foot back"
     sleep (0.5)
     puts "Left foot back"
@@ -9,10 +8,21 @@ def first_steps
   end
 
 def a_few_more_steps
-  # Write a loop that outputs the first two sets of steps in the Two-Step
+  puts "Right foot back"
+    sleep (0.5)
+    puts "Left foot back"
+    sleep (0.5)
+    puts "Stop"
+    sleep(1)
+    puts "Right foot steps right and back"
+    puts "Left foot crosses over right"
+    puts "Right foot steps right"
+    sleep
+  pu# Write a loop that outputs the first two sets of steps in the Two-Step
 end
 
 def how_many_steps?
+  steps = log10loop dosteps +=log10loop
   # Write a loop that fulfills all criteria from Part 3 of the README.md
 end
 
